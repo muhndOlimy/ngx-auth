b429e77b6aab9a5c057e29c4e27708ffce8359b4
muhndOlimy <muhnd.olimy@kortobaa.com>
2022-06-29 10:18:29 +0200

    create:intialize ng-auth library structure

---
+++ projects/ngx-auth/public-api.ts
@@ -0,0 +1,16 @@
+/*
+ * Public API Surface of ngx-auth
+ */
+
+export * from './login/login.module';
+export * from './login/login.service';
+
+export * from './register/register.module';
+export * from './register/register.service';
+
+export * from './user/user.module';
+export * from './user/user.service';
+
+export * from './verify/verify.module';
+export * from './verify/verify.service';
+
