2bca728716df42646d3103831c844f6e12d76c37
muhndOlimy <muhnd.olimy@kortobaa.com>
2022-06-29 13:28:48 +0200

    add: user interface

---
+++ projects/ngx-auth/_helper/user.interface.ts
@@ -0,0 +1,13 @@
+export interface User {
+    id:number
+    username:string
+    email:string
+    phone:string
+    photoUrl:string
+    roles:string[]
+    additionalData:object
+    userId?:number
+    token?:string
+    refreshDate?:string
+    refreshToken?:string
+}
