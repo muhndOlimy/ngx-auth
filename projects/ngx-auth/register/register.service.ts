54e7bdf506cb5c793e93baeb8f16a9c3c139bd09
muhndOlimy <muhnd.olimy@kortobaa.com>
2022-07-03 16:53:15 +0200

    add: register & verify methods

--- projects/ngx-auth/register/register.service.ts
+++ projects/ngx-auth/register/register.service.ts
@@ -1,9 +1,81 @@
-import { Injectable } from '@angular/core';
+import { Inject, Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+export interface RegisterData{
+  email:string
+  password:string
+  phone:string
+  username:string
+  roleId:number
+}
 
 @Injectable({
   providedIn: 'root'
 })
 export class RegisterService {
 
-  constructor() { }
+  verifySetting:{
+    defaultVerifyType:string
+    verifyTypes:string[]
+    verifyUser:string
+  }
+
+  constructor(private _http:HttpClient , @Inject("ngx-auth") private AuthConfig:{API_URL:string}) {
+    this.getVerifySetting()
+  }
+
+  //Create promise to consumer resolve by success message or reject by error.
+  async register(data:RegisterData):Promise<string>{
+    
+    return new Promise(async (resolve, reject) => {
+
+      try{
+        await this.processRegister(data);
+        resolve("user is registered successfully");
+      }
+      catch(err){
+        reject(err)
+      }
+    })
+  }
+
+  //Request register as promise
+  private processRegister(data:RegisterData):Promise<any>{
+    return this._http.post(`${this.AuthConfig.API_URL}users` , data).toPromise();
+  }
+
+  //Create promise to consumer resolve by success message or reject by error.
+  async verify(data:string):Promise<string>{
+    return new Promise(async (resolve, reject) => {
+
+      try{
+        await this.processVerify(data);
+        resolve("user account is verified successfully");
+      }
+      catch(err){
+        reject(err)
+      }
+    })
+  }
+
+  //Request verify as promise
+  private processVerify(data:string):Promise<any>{
+    return this._http.post(`${this.AuthConfig.API_URL}users/register-verify` , {token:data}).toPromise();
+  }
+
+  //Create promise to consumer resolve by success message or reject by error.
+  private async getVerifySetting():Promise<void>{
+    try{
+      let response = await this.processVerifySetting();
+      this.verifySetting = response;
+    }
+    catch{
+      //do nothing 
+    }
+  }
+
+  //Request verify as promise
+  private processVerifySetting():Promise<any>{
+    return this._http.get(`${this.AuthConfig.API_URL}verification-settings`).toPromise();
+  }
 }
