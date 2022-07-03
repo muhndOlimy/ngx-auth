8931b091e40e29339fd4f9ca4735c79ae699e3b0
muhndOlimy <muhnd.olimy@kortobaa.com>
2022-07-03 16:52:03 +0200

    add: register as sub enrty point

--- projects/ngx-auth/public-api.ts
+++ projects/ngx-auth/public-api.ts
@@ -2,9 +2,6 @@
  * Public API Surface of ngx-auth
 */
 
-export * from './register/register.module';
-export * from './register/register.service';
-
 export * from './user/user.module';
 export * from './user/user.service';
 export * from "./user/User";
