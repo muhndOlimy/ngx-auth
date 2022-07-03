db6a1ab53f9917cbd879c6b4132cbfdfa9be166c
muhndOlimy <muhnd.olimy@kortobaa.com>
2022-07-03 16:58:13 +0200

    delete: verfiy service and module

--- projects/ngx-auth/public-api.ts
+++ projects/ngx-auth/public-api.ts
@@ -6,7 +6,4 @@ export * from './user/user.module';
 export * from './user/user.service';
 export * from "./user/User";
 
-export * from './verify/verify.module';
-export * from './verify/verify.service';
-
 
