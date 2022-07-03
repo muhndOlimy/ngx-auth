90ebab2abe4170474c65057f7c07c087c5b6db7d
muhndOlimy <muhnd.olimy@kortobaa.com>
2022-07-03 10:38:40 +0200

    update: loginservice login method

--- projects/ngx-auth/login/login.service.ts
+++ projects/ngx-auth/login/login.service.ts
@@ -1,6 +1,6 @@
-import { Injectable } from '@angular/core';
+import { Inject, Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { UserService , API_URL} from 'ngx-auth';
+import { UserService , User} from 'ngx-auth';
 
 interface LoginData{
   login:string
@@ -13,34 +13,34 @@ interface LoginData{
 export class LoginService {
 
 
-  constructor(private _http:HttpClient , private _user:UserService) {}
+  constructor(private _http:HttpClient , private _user:UserService , @Inject("ngx-auth") private AuthConfig:{API_URL:string}) {}
 
   //Create promise to consumer resolve by success message or reject by error.
-  login(data:LoginData):Promise<string>{
+  async login(data:LoginData):Promise<string>{
     
-    return new Promise((resolve, reject) => {
+    return new Promise(async (resolve, reject) => {
 
-      this.processLogin(data)
-      .then(data=>{
-        this.handleLogin(data)
-        resolve("User is logged in")
-      })
-      .catch(err=>{
+      try{
+        let response = await this.processLogin(data);
+        this.handleLogin(response);
+        resolve("User is logged in");
+      }
+      catch(err){
         reject(err)
-      })
- 
+      }
     })
   }
 
   //Request login as promise
   private processLogin(data:LoginData):Promise<any>{
-    return this._http.post(`${API_URL}users/login` , data).toPromise();
+    return this._http.post(`${this.AuthConfig.API_URL}users/login` , data).toPromise();
   }
 
   //Handle success login
-  private handleLogin(data):void{
+  private handleLogin(data:User):void{
     localStorage.setItem("Token" , data.token)
-    localStorage.setItem("RefreshToken" , data.refreshToken)   
+    localStorage.setItem("RefreshToken" , data.refreshToken)
+    localStorage.setItem("ExpiryDate" , data.expiryDate)
     this._user.currentUser = {
       id:data.id,
       username:data.username,
